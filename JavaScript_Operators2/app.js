a9adb21688e8a0106b6c73aeb8df9c08b96f60e6
mattymatt1994 <121513907+mattymatt1994@users.noreply.github.com>
2023-03-13 15:38:22 -0400

    Add files via upload

---
+++ JavaScript_Operators2/app.js
@@ -0,0 +1,74 @@
+console.log("Hello World!\n==========\n");
+console.log(
+  "Follow the steps in the README.md file to complete the exercises:\n==========\n"
+);
+
+// Exercise 1
+
+var a = 20;
+var b = 4;
+var add = a + b;
+var minus = a - b;
+var multiply = a * b;
+var divide = a / b;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+
+console
+// Exercise 2
+
+
+
+//  "1111"
+// "11eleven"
+// 12
+//"Frodo11"
+//"True11"
+//"FrodoBaggins"
+
+let num = 11
+let str = "11"
+let str2 = "eleven"
+let isPresent = true
+let firstName = "Frodo"
+let lastName = "Baggins"
+console.log(num + str);
+console.log(num + str2);
+console.log(num + isPresent);
+console.log(firstName + num);
+console.log(isPresent + str);
+console.log(firstName +" " + lastName);
+// Exercise 3
+console.log("EXERCISE 3:\n==========\n");
+
+// YOUR CODE HERE
+let val = 5;
+let str3 = "5";
+let str4 = "five";
+let isPresent2 = false;
+
+console.log(true);
+console.log(false);
+console.log(true)
+console.log(false);
+console.log(true);
+console.log(true);
+console.log(true);
+console.log(NaN === false);
+console.log(false);
+console.log(true);
+console.log(false);
+/*True
+  *False
+  *True
+  *False
+  *True
+  *True
+  *True
+  *NaN === false
+  *false
+  *true
+  */
